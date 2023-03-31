c40729518e4b68148b7b60086cae842b0adf2c55
manaccac <maximenaccache@gmail.com>
2023-03-31 14:05:11 +0200

    maj depth 6

--- include/algo.hpp
+++ include/algo.hpp
@@ -5,24 +5,62 @@
 # include <string.h>
 #include <chrono>
 #include <vector>
+#include <unordered_map>
 #include <utility>
 // # include "game.hpp"
+
+// TranspositionTable class
+class TranspositionTable {
+public:
+    struct TableEntry {
+        int depth;
+        int value;
+        int flag;
+    };
+
+    enum EntryFlag {
+        EXACT, LOWERBOUND, UPPERBOUND
+    };
+
+    void store(int hash_key, int value, int depth, int flag) {
+        TableEntry entry{depth, value, flag};
+        table[hash_key] = entry;
+    }
+
+    TableEntry* get(int hash_key) {
+        auto it = table.find(hash_key);
+        if (it != table.end()) {
+            return &it->second;
+        }
+        return nullptr;
+    }
+
+private:
+    std::unordered_map<int, TableEntry> table;
+};
 
 class Algo {
 	public:
 		Algo() { }
+		TranspositionTable transposition_table;
+
 
 		std::vector<std::pair<int, int>> get_windows(char *map, int mapSize, int margin) ;
 
 		int coup(char *map, bool joueur);
 
-		int test_main(char *map, int mapSize, int player, int black_score, int white_score);
+		int test_main(char *map, int mapSize, bool player, int black_score, int white_score);
 
-		int minimax_v2(char *map, int depth, bool EstMax, int alpha, int beta);
+		int minimax_v2(char *map, int depth, bool EstMax, int alpha, int beta, char joueur, char adversaire);
 		int evaluerPosition_line(char *grille, char joueur);
 		int evaluerLigne(int ligne_score, char joueur);
 
-		int mtd_f(char *map, int depth, int f);
+		int mtd_f(char *map, int depth, int f, char joueur, char adversaire);
+
+		int count_tokens_in_direction(char *grille, char joueur, int i, int j, int dx, int dy);
+		int evaluerPosition_line_joueur1(char *grille);
+		int evaluerPosition_line_joueur2(char *grille);
+		std::vector<std::pair<int, int>> get_adjacent_positions(char *map, int mapSize);
 
 	private:
 		// Game	game;
--- src/algo.cpp
+++ src/algo.cpp
@@ -2,128 +2,81 @@
 
 // map size
 #define N 9
+#define SCORE_THRESHOLD 500
 
-
-std::vector<std::pair<int, int>> Algo::get_windows(char *map, int mapSize, int margin) {
-    int x_min = mapSize, x_max = 0, y_min = mapSize, y_max = 0;
+std::vector<std::pair<int, int>> Algo::get_adjacent_positions(char *map, int mapSize) {
+    std::vector<std::pair<int, int>> adjacent_positions;
 
     for (int i = 0; i < mapSize; i++) {
         for (int j = 0; j < mapSize; j++) {
             if (map[i * mapSize + j] != '0') {
-                x_min = std::min(x_min, i);
-                x_max = std::max(x_max, i);
-                y_min = std::min(y_min, j);
-                y_max = std::max(y_max, j);
+                for (int dx = -1; dx <= 1; dx++) {
+                    for (int dy = -1; dy <= 1; dy++) {
+                        if (dx == 0 && dy == 0) continue;
+                        int x = i + dx;
+                        int y = j + dy;
+                        if (x >= 0 && x < mapSize && y >= 0 && y < mapSize && map[x * mapSize + y] == '0') {
+                            adjacent_positions.push_back({x, y});
+                        }
+                    }
+                }
             }
         }
     }
 
-    x_min = std::max(x_min - margin, 0);
-    x_max = std::min(x_max + margin, mapSize - 1);
-    y_min = std::max(y_min - margin, 0);
-    y_max = std::min(y_max + margin, mapSize - 1);
+    // Supprimer les doublons
+    std::sort(adjacent_positions.begin(), adjacent_positions.end());
+    adjacent_positions.erase(std::unique(adjacent_positions.begin(), adjacent_positions.end()), adjacent_positions.end());
 
-    return { {x_min, y_min}, {x_max, y_max} };
+    return adjacent_positions;
 }
 
+
+int Algo::evaluerPosition_line_joueur1(char *grille) {
+    return evaluerPosition_line(grille, '1');
+}
+
+int Algo::evaluerPosition_line_joueur2(char *grille) {
+    return evaluerPosition_line(grille, '2');
+}
+
+
+int Algo::count_tokens_in_direction(char *grille, char joueur, int i, int j, int dx, int dy) {
+    int count = 0;
+    int x = i, y = j;
+
+    while (0 <= x && x < N && 0 <= y && y < N && grille[x * N + y] == joueur) {
+        count++;
+        x += dx;
+        y += dy;
+    }
+
+    return count;
+}
 
 int Algo::evaluerPosition_line(char *grille, char joueur) {
     int score = 0;
     char adversaire = (joueur == '1') ? '2' : '1';
-    int captures = 0;
 
     // Directions : horizontale, verticale, diagonale droite, diagonale gauche
     int direction[4][2] = {{1, 0}, {0, 1}, {1, 1}, {1, -1}};
 
+    // Scores pour différentes longueurs de lignes
+    int ligne_scores[6] = {0, 1, 10, 100, 1000, 100000};
+
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < N; j++) {
-            if (grille[i * N + j] == joueur) {
+            if (grille[i * N + j] == joueur || grille[i * N + j] == adversaire) {
                 for (int d = 0; d < 4; ++d) {
-                    int count = 1;
+                    int count = count_tokens_in_direction(grille, grille[i * N + j], i, j, direction[d][0], direction[d][1]);
 
-                    for (int k = 1; k < 5; ++k) {
-                        int x = i + k * direction[d][0];
-                        int y = j + k * direction[d][1];
-                        if (0 <= x && x < N && 0 <= y && y < N) {
-                            if (grille[x * N + y] == joueur) {
-                                count += 1;
-                            } else {
-                                break;
-                            }
-                        } else {
-                            break;
-                        }
-                    }
-					// Vérifier si l'adversaire peut être bloqué dans cette direction
-					if (count <= 2) {
-						for (int k = 1; k < 5; ++k) {
-							int x = i + k * direction[d][0];
-							int y = j + k * direction[d][1];
-							if (0 <= x && x < N && 0 <= y && y < N) {
-								if (grille[x * N + y] == adversaire) {
-									bool blocked = true;
-									for (int m = k + 1; m < 5; ++m) {
-										int x_block = i + m * direction[d][0];
-										int y_block = j + m * direction[d][1];
-										if (0 <= x_block && x_block < N && 0 <= y_block && y_block < N) {
-											if (grille[x_block * N + y_block] == joueur) {
-												blocked = false;
-												break;
-											}
-										} else {
-											break;
-										}
-									}
-									if (blocked) {
-										score -= 10; // ajouter un score négatif pour bloquer l'adversaire
-									}
-									break;
-								}
-							} else {
-								break;
-							}
-						}
-					}
-					if (count == 5)
-						return INT_MAX;
-                    score += evaluerLigne(count, joueur);
-                }
-            } 
-			else if (grille[i * N + j] == adversaire) {
-                for (int d = 0; d < 4; ++d) {
-                    int count = 0;
-                    bool is_valid = false;
-                    int x1 = i + direction[d][0];
-                    int y1 = j + direction[d][1];
-                    int x2 = i - direction[d][0];
-                    int y2 = j - direction[d][1];
-                    if (0 <= x1 && x1 < N && 0 <= y1 && y1 < N &&
-                        0 <= x2 && x2 < N && 0 <= y2 && y2 < N) {
-                        if (grille[x1 * N + y1] == adversaire &&
-                            grille[x2 * N + y2] == joueur) {
-                            for (int k = 3; k <= 4; ++k) {
-                                int x = i + k * direction[d][0];
-                                int y = j + k * direction[d][1];
-                                if (0 <= x && x < N && 0 <= y && y < N) {
-                                    if (grille[x * N + y] == '0') {
-                                        is_valid = true;
-                                        captures += 1;
-                                        break;
-                                    }
-                                } else {
-                                    break;
-                                }
-                            }
-                        }
-                    }
-                    if (is_valid) {
-                        break;
-                    }
+                    // Ajouter le score correspondant
+                    score += (grille[i * N + j] == joueur ? 1 : -1) * ligne_scores[count];
                 }
             }
         }
     }
-    score += captures * 2000; // Ajouter les points de capture
+
     return score;
 }
 
@@ -151,7 +104,7 @@ int Algo::evaluerLigne(int ligne_score, char joueur) {
 
 
 
-int Algo::test_main(char *map, int mapSize, int player, int black_score, int white_score) {
+int Algo::test_main(char *map, int mapSize, bool player, int black_score, int white_score) {
 	// je veux qu'il calcule le score de la map en fonction du player
 
 	// dprintf(1, "back_score = %d\nwhite_score = %d\n", black_score, white_score);
@@ -162,7 +115,7 @@ int Algo::test_main(char *map, int mapSize, int player, int black_score, int whi
 	char temp_player;
 	char other_player;
 
-	if (player == 1){
+	if (player == true){
 		temp_player = '2';
 		other_player = '1';
 	}
@@ -171,34 +124,40 @@ int Algo::test_main(char *map, int mapSize, int player, int black_score, int whi
 		other_player = '2';
 	}
 	if (black_score == INT_MAX || white_score == INT_MAX)
-		return INT_MAX;
+        return INT_MAX;
 
-	score = evaluerPosition_line(map, temp_player);
-	if (score == INT_MAX){
-		return INT_MAX;
-	}
-	score -= evaluerPosition_line(map, other_player);
-	if (score == INT_MAX) {
-		return INT_MAX;
-	}
+    score = player ? evaluerPosition_line_joueur2(map) : evaluerPosition_line_joueur1(map);
+
+    if (score == INT_MAX){
+        return INT_MAX;
+    }
+    score -= player ? evaluerPosition_line_joueur1(map) : evaluerPosition_line_joueur2(map);
+    if (evaluerPosition_line(map, other_player) == INT_MAX) {
+        score = INT_MIN;
+        return INT_MIN;
+    }
+
 
 	return score;
 }
 
-int Algo::mtd_f(char *map, int depth, int f) {
+int Algo::mtd_f(char *map, int depth, int f, char joueur, char adversaire) {
     int g = f;
     int beta;
     int upperBound = INT_MAX;
     int lowerBound = INT_MIN;
+	// count = 0;
+	static int call_count = 0;
 
     while (lowerBound < upperBound) {
+		call_count++;
         if (g == lowerBound) {
             beta = g + 1;
         } else {
             beta = g;
         }
 
-        g = minimax_v2(map, depth, true, beta - 1, beta);
+        // g = minimax_v2(map, depth, true, beta - 1, beta);
 
         if (g < beta) {
             upperBound = g;
@@ -211,16 +170,36 @@ int Algo::mtd_f(char *map, int depth, int f) {
 }
 
 
-int Algo::minimax_v2(char *map, int depth, bool EstMax, int alpha, int beta) {
+int Algo::minimax_v2(char *map, int depth, bool EstMax, int alpha, int beta, char joueur, char adversaire) {
     // je veux une variable globale qui compte le nombre de fois que la fonction est appelée
     static int call_count = 0;
     call_count++;
+
+	// Generate hash key for the current position
+    int hash_key = std::hash<std::string>{}(std::string(map, N * N));
+
+    // Check if the position is in the transposition table
+    TranspositionTable::TableEntry* entry = transposition_table.get(hash_key);
+    if (entry != nullptr && entry->depth >= depth) {
+        if (entry->flag == TranspositionTable::EXACT) {
+            return entry->value;
+        } else if (entry->flag == TranspositionTable::LOWERBOUND && entry->value > alpha) {
+            alpha = entry->value;
+        } else if (entry->flag == TranspositionTable::UPPERBOUND && entry->value < beta) {
+            beta = entry->value;
+        }
+
+        if (alpha >= beta) {
+            return entry->value;
+        }
+    }
+
 
     if (test_main(map, N, true, 0, 0) == INT_MAX) {
         return INT_MAX;
     }
 
-    else if (test_main(map, N, false, 0, 0) == INT_MAX) {
+    else if (test_main(map, N, false, 0, 0) == INT_MIN) {
         return INT_MIN;
     }
 
@@ -228,20 +207,19 @@ int Algo::minimax_v2(char *map, int depth, bool EstMax, int alpha, int beta) {
         return test_main(map, N, true, 0, 0);
     }
 
-    int score;
+    int meilleur_score;
     if (EstMax) {
-        int meilleur_score = INT_MIN;
+        meilleur_score = INT_MIN;
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < N; j++) {
                 if (map[i * N + j] == '0') {
-                    map[i * N + j] = '2'; // 2 pour le moment
-                    score = minimax_v2(map, depth - 1, false, alpha, beta);
+                    map[i * N + j] = joueur; // 2 pour le moment
+                    int score = minimax_v2(map, depth - 1, false, alpha, beta, joueur, adversaire);
                     map[i * N + j] = '0';
-					if (score == INT_MAX) {
-						return INT_MAX;
-					}
+					if (score - alpha >= SCORE_THRESHOLD) {
+                        return score;
+                    }
                     meilleur_score = std::max(meilleur_score, score);
-
                     alpha = std::max(alpha, meilleur_score);
                     if (beta <= alpha) { // Élagage bêta
                         return meilleur_score;
@@ -249,18 +227,19 @@ int Algo::minimax_v2(char *map, int depth, bool EstMax, int alpha, int beta) {
                 }
             }
         }
-        return meilleur_score;
     }
     else {
-        int meilleur_score = INT_MAX;
+        meilleur_score = INT_MAX;
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < N; j++) {
                 if (map[i * N + j] == '0') {
-                    map[i * N + j] = '1'; // 1 pour le moment
-                    score = minimax_v2(map, depth - 1, true, alpha, beta);
+                    map[i * N + j] = adversaire; // 1 pour le moment
+                    int score = minimax_v2(map, depth - 1, true, alpha, beta, joueur, adversaire);
                     map[i * N + j] = '0';
+					if (beta - score >= SCORE_THRESHOLD) {
+                        return score;
+                    }
                     meilleur_score = std::min(meilleur_score, score);
-
                     beta = std::min(beta, meilleur_score);
                     if (beta <= alpha) { // Élagage alpha
                         return meilleur_score;
@@ -268,74 +247,97 @@ int Algo::minimax_v2(char *map, int depth, bool EstMax, int alpha, int beta) {
                 }
             }
         }
-        return meilleur_score;
     }
+	int flag;
+    if (meilleur_score <= alpha) {
+        flag = TranspositionTable::UPPERBOUND;
+    } else if (meilleur_score >= beta) {
+        flag = TranspositionTable::LOWERBOUND;
+    } else {
+        flag = TranspositionTable::EXACT;
+    }
+    transposition_table.store(hash_key, meilleur_score, depth, flag);
+    return meilleur_score;
+}
+
+std::vector<std::pair<int, int>> Algo::get_windows(char *map, int mapSize, int margin) {
+    int x_min = mapSize, x_max = 0, y_min = mapSize, y_max = 0;
+
+    for (int i = 0; i < mapSize; i++) {
+        for (int j = 0; j < mapSize; j++) {
+            if (map[i * mapSize + j] != '0') {
+                x_min = std::min(x_min, i);
+                x_max = std::max(x_max, i);
+                y_min = std::min(y_min, j);
+                y_max = std::max(y_max, j);
+            }
+        }
+    }
+
+    x_min = std::max(x_min - margin, 0);
+    x_max = std::min(x_max + margin, mapSize - 1);
+    y_min = std::max(y_min - margin, 0);
+    y_max = std::min(y_max + margin, mapSize - 1);
+
+    return { {x_min, y_min}, {x_max, y_max} };
 }
 
 
 int Algo::coup(char *map, bool joueur) {
-	// static coup_count = 0;
-
-	static int call_count = 0;
-    int meilleur_coup;
+    dprintf(1, "joueur = %d", joueur);
     int mapSize = N;
-    int score = 0;
-    char pion;
-    dprintf(1, "joueur = %d\n", joueur);
+    int meilleurScore = INT_MIN;
+    int meilleurX = -1;
+    int meilleurY = -1;
+    int profondeurMax = 6; // Ajustez la profondeur maximale selon vos besoins
 
-    if (joueur == true)
-        pion = '2';
-    else 
-        pion = '1';
+    // Utiliser les positions adjacentes
+    std::vector<std::pair<int, int>> adjacent_positions = get_adjacent_positions(map, mapSize);
 
-    int meilleur_score = pion == '1' ? INT_MAX : INT_MIN;
-
-    // Obtenez les fenêtres rectangulaires
-    int margin = 2;
-    std::vector<std::pair<int, int>> windows = get_windows(map, mapSize, margin);
-    int x_min = windows[0].first;
-    int y_min = windows[0].second;
-    int x_max = windows[1].first;
-    int y_max = windows[1].second;
-    // Si x_min et y_min sont proches et que x_max et y_max sont proches
-    if (call_count == 0) {
-		dprintf(1, "lqqqq\n");
-		dprintf(1, "x_min = %d, y_min = %d, x_max = %d, y_max = %d \n", x_min, y_min, x_max, y_max);
-        // Se concentrer sur un carré de 5x5 au milieu de la carte
-        int mid_x = mapSize / 2;
-        int mid_y = mapSize / 2;
-        x_min = std::max(mid_x - 2, 0);
-        y_min = std::max(mid_y - 2, 0);
-        x_max = std::min(mid_x + 2, mapSize - 1);
-        y_max = std::min(mid_y + 2, mapSize - 1);
+    if (adjacent_positions.empty()) {
+        // S'il n'y a pas de pions sur le plateau, jouer au centre du plateau
+        int center = mapSize / 2;
+        map[center * mapSize + center] = joueur ? '2' : '1';
+        return center * mapSize + center;
     }
-	call_count++;
 
-    for (int i = x_min; i <= x_max; i++) {
-        for (int j = y_min; j <= y_max; j++) {
+    for (int profondeur = 1; profondeur <= profondeurMax; profondeur++) {
+        int current_best_score = INT_MIN;
+        int current_best_x = -1;
+        int current_best_y = -1;
+
+        for (auto &pos : adjacent_positions) {
+            int i = pos.first;
+            int j = pos.second;
+
             if (map[i * mapSize + j] == '0') {
-                map[i * mapSize + j] = pion;
-                if (pion == '1') {
-                    score = mtd_f(map, 2, 0);
-                }
-				else {
-                    score = mtd_f(map, 2, 0);
-                }
-				if (pion == '1' && score == INT_MAX) {
-					return meilleur_coup;
-				}
-				if (pion == '2' && score == INT_MIN) {
-					return meilleur_coup;
-				}
+                map[i * mapSize + j] = joueur ? '2' : '1';
 
-                map[i * mapSize + j] = '0';
-                if ((pion == '1' && score < meilleur_score) || (pion == '2' && score > meilleur_score)) {
-                    meilleur_score = score;
-                    meilleur_coup = (i * mapSize + j);
+                int score = minimax_v2(map, profondeur - 1, !joueur, INT_MIN, INT_MAX, joueur ? '2' : '1', joueur ? '1' : '2');
+                if (score > current_best_score) {
+                    current_best_score = score;
+                    current_best_x = i;
+                    current_best_y = j;
                 }
+
+                // Réinitialiser la position pour le prochain essai
+                map[i * mapSize + j] = '0';
             }
         }
+
+        if (current_best_score > meilleurScore) {
+            meilleurScore = current_best_score;
+            meilleurX = current_best_x;
+            meilleurY = current_best_y;
+        }
+
+        // Si vous avez atteint un score maximal ou minimal, arrêtez l'Iterative Deepening
+        if (meilleurScore == INT_MAX || meilleurScore == INT_MIN) {
+            break;
+        }
     }
-    map[meilleur_coup] = pion;
-    return meilleur_coup;
+
+    map[meilleurX * mapSize + meilleurY] = joueur ? '2' : '1';
+    return meilleurScore;
 }
+
